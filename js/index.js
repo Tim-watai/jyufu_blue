ebc7fa6235353a832bd8ca67b0a5a7687b5b27fc
Tim-watai <59860909+Tim-watai@users.noreply.github.com>
2020-03-02 11:29:04 +0800

    windows update
    www

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-    console.log("hello This is {Pony all.js}");
-   
+    console.log("index ready");
+
+    $('li.hot_btn').click(
+        function(){
+            if($('aside.hot_windows').hasClass('active') != true){
+                $('aside.hot_windows').removeClass('disabled');
+                $('aside.hot_windows').addClass('active');
+                return
+           }
+        }
+    );
+   $('article.bg span.close').click(
+     function(){
+       
+        if($('aside.hot_windows').hasClass('active') == true){
+              $('aside.hot_windows').removeClass('active');
+              $('aside.hot_windows').addClass('disabled');
+              return
+         }
+     }
+
+   );
 
 
 });//total
