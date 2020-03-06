e5f1d3d8a1f980bb7e3dbab02547fe7fa9cea474
Tim-watai <59860909+Tim-watai@users.noreply.github.com>
2020-03-06 18:21:47 +0800

    after_06:00
    e

--- js/index.js
+++ js/index.js
@@ -1,46 +1,71 @@
-$(document).ready(function(){
-    console.log("index ready");
+$(document).ready(function() {
+  console.log("index ready");
 
-    $('li.hot_btn').click(
-        function(){
-            if($('aside.hot_windows').hasClass('abled') != true){
-                $('aside.hot_windows').removeClass('disabled');
-                $('aside.hot_windows').addClass('abled');
-                return
-           }
+  $("li.hot_btn").click(function() {
+    if ($("aside.hot_windows").hasClass("abled") != true) {
+      $("aside.hot_windows").removeClass("disabled");
+      $("aside.hot_windows").addClass("abled");
+      return;
+    }
+  });
+  $("article.bg span.close").click(function() {
+    if ($("aside.hot_windows").hasClass("abled") == true) {
+      $("aside.hot_windows").removeClass("abled");
+      $("aside.hot_windows").addClass("disabled");
+      return;
+    }
+  });
+
+  $("li.message_icon").click(function() {
+    if ($("aside.title_windows").hasClass("abled") != true) {
+      $("aside.title_windows").removeClass("disabled");
+      $("aside.title_windows").addClass("abled");
+      return;
+    }
+  });
+  $("aside.title_windows div.close").click(function() {
+    if ($("aside.title_windows").hasClass("abled") == true) {
+      $("aside.title_windows").removeClass("abled");
+      $("aside.title_windows").addClass("disabled");
+      return;
+    }
+  });
+
+  $('main article.menu aside.btn_group button').click(
+    function () {
+        if($(this).hasClass('hot') == false){
+            $(this).addClass('hot');
+            $(this).siblings().removeClass('hot');
         }
-    );
-   $('article.bg span.close').click(
-     function(){
-       
-        if($('aside.hot_windows').hasClass('abled') == true){
-              $('aside.hot_windows').removeClass('abled');
-              $('aside.hot_windows').addClass('disabled');
-              return
-         }
-     }
-   );
 
-
-   $('li.message_icon').click(
-       function(){
-        if($('aside.title_windows').hasClass('abled') != true){
-            $('aside.title_windows').removeClass('disabled');
-            $('aside.title_windows').addClass('abled');
-            return
-       }
-       }
-   );
-    $('aside.title_windows div.close').click(
-        function(){
-         
-           if($('aside.title_windows').hasClass('abled') == true){
-                 $('aside.title_windows').removeClass('abled');
-                 $('aside.title_windows').addClass('disabled');
-                 return
-            }
+        if($(this).hasClass('today') === true)
+        {
+           $('main article.menu ul.today').css('display','block');
+           $('main article.menu ul.run_ball').css('display','none');
+           $('main article.menu ul.early').css('display','none');
+           $('main article.menu ul.double').css('display','none');
         }
+        else if($(this).hasClass('run_ball') === true){
+            $('main article.menu ul.today').css('display','none');
+            $('main article.menu ul.run_ball').css('display','block');
+            $('main article.menu ul.early').css('display','none');
+            $('main article.menu ul.double').css('display','none');
+        }
+        else if($(this).hasClass('early') === true){
+            $('main article.menu ul.today').css('display','none');
+            $('main article.menu ul.run_ball').css('display','none');
+            $('main article.menu ul.early').css('display','block');
+            $('main article.menu ul.double').css('display','none');
+        }
+        else if($(this).hasClass('double') === true){
+            $('main article.menu ul.today').css('display','none');
+            $('main article.menu ul.run_ball').css('display','none');
+            $('main article.menu ul.early').css('display','none');
+            $('main article.menu ul.double').css('display','block');
+        }
+    }
    
-      );
+);
 
-});//total
+
+}); //total
--- js/market.js
+++ js/market.js
@@ -10,5 +10,28 @@ $('nav.kinds ul li').click(
     }
 );
 
+$('main.market aside article.option div.mode span').click(
+    function () {
+        if($(this).hasClass('active') == false){
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+        }
+
+        if($(this).hasClass('beginner') === true)
+        {
+            $('main.market aside ul li.beginner').css('display','block');
+            $('main.market aside ul li.senior').css('display','none');
+        }
+        else if($(this).hasClass('senior') === true)
+        {
+            $('main.market aside ul li.senior').css('display','block');
+            $('main.market aside ul li.beginner').css('display','none');
+        }
+
+    }
+
+);
+
+
 
 });//total
