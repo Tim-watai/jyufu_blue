b5010a746cf8ce1f444ce7fa5eb4008e054d672c
MingJingShu <a0911077842@gmail.com>
2020-03-24 22:07:04 +0800

    change 投注紀錄

--- js/index.js
+++ js/index.js
@@ -130,7 +130,16 @@ $(document).ready(function() {
       return $(this).removeClass('active');   
     }
   });
-  //首頁_關閉全部彈窗
+  //切換_我的視窗_投注紀錄
+  $('aside.my_windows_bet_record div.header ul li.back , aside.my_windows div.group ul li.my_rd').click(function(){
+    console.log('mb');
+    if($('aside.my_windows_bet_record').hasClass('active') !== true)
+    {
+      return $('aside.my_windows_bet_record').addClass('active');    
+    }else{
+      return $('aside.my_windows_bet_record').removeClass('active');   
+    }
+  });
 
 
 
