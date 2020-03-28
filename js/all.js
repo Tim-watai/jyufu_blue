ea6bcc4477c8f2136d4a0dc72b03aaff33d99fce
MingJingShu <a0911077842@gmail.com>
2020-03-28 17:52:12 +0800

    改位置

--- js/all.js
+++ js/all.js
@@ -2,26 +2,5 @@ $(document).ready(function(){
     console.log("all.js");
    
 
-//double賽事串關
 
-
-
-$('.double_mode').click(
-    function (){
-       
-        if($('.window_double_mode').hasClass('active') === true){
-              $('.window_double_mode').removeClass('active');
-             return $('.window_double_mode .hide_area').slideToggle(300,'linear');
-            
-        }else{
-             $('.window_double_mode').addClass('active');
-             return $('.window_double_mode .hide_area').slideToggle(300,'linear');
-        }
-
-
-
-
-        // console.log('click');
-        // $('.window_double_mode .hide_area').slideToggle(300,'linear');
-    })
 });//total
---
+++ js/market/double/double.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    console.log("double.js");
+   
+
+//double賽事串關
+
+
+
+$('.double_mode').click(
+    function (){
+       
+        if($('.window_double_mode').hasClass('active') === true){
+              $('.window_double_mode').removeClass('active');
+             return $('.window_double_mode .hide_area').slideToggle(300,'linear');
+            
+        }else{
+             $('.window_double_mode').addClass('active');
+             return $('.window_double_mode .hide_area').slideToggle(300,'linear');
+        }
+
+
+
+
+        // console.log('click');
+        // $('.window_double_mode .hide_area').slideToggle(300,'linear');
+    })
+});//total
