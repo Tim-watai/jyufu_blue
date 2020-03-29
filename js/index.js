ef1b761875a705c093d791ddfc18f926a5c2e61a
MingJingShu <a0911077842@gmail.com>
2020-03-29 16:10:27 +0800

    分離投注紀錄並改變與Header的層次

--- js/index.js
+++ js/index.js
@@ -176,15 +176,15 @@ $('aside.my_windows_card div.header ul li.back , aside.my_windows_mb div.mb_cont
 
 
   //切換_我的視窗_投注紀錄
-  $('aside.my_windows_bet_record div.header ul li.back , aside.my_windows div.group ul li.my_rd').click(function(){
-    console.log('mb');
-    if($('aside.my_windows_bet_record').hasClass('active') !== true)
-    {
-      return $('aside.my_windows_bet_record').addClass('active');    
-    }else{
-      return $('aside.my_windows_bet_record').removeClass('active');   
-    }
-  });
+  // $('aside.my_windows_bet_record div.header ul li.back , aside.my_windows div.group ul li.my_rd').click(function(){
+  //   console.log('mb');
+  //   if($('aside.my_windows_bet_record').hasClass('active') !== true)
+  //   {
+  //     return $('aside.my_windows_bet_record').addClass('active');    
+  //   }else{
+  //     return $('aside.my_windows_bet_record').removeClass('active');   
+  //   }
+  // });
 
 
 
