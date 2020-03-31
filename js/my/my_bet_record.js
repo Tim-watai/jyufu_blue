ac5fbcd43520c33ade59abc625e919bebad77db6
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-03-31 15:13:42 +0800

    更改路徑 剩下取款頁面

--- js/my/my_bet_record.js
+++ js/my/my_bet_record.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
     console.log("my_bet_record.js");
-    //切換_我的視窗_投注紀錄
-  // $('aside.my_windows_bet_record div.header ul li.back , aside.my_windows div.group ul li.my_rd').click(function(){
-  //   console.log('mb');
-  //   if($('aside.my_windows_bet_record').hasClass('active') !== true)
-  //   {
-  //     return $('aside.my_windows_bet_record').addClass('active');    
-  //   }else{
-  //     return $('aside.my_windows_bet_record').removeClass('active');   
-  //   }
-  // });
-
-
+ 
 });//total
--- js/my_windows_transfer.js
+++ js/my_windows_transfer.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     console.log("my_windows_transfer.js");
-
+//轉帳頁面的轉帳彈窗
     $('aside.my_windows_mb_transfer div.content div.groups , aside.my_windows_mb_transfer_doing .windows div.content button').click(
         function (){
             console.log("my_windows_transfer.js");
