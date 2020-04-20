4c328c79cc4cdb5b5b660cb70b61fb4bdf49271c
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-04-20 18:12:40 +0800

    更改了 串關彈窗

--- js/my_swiper.js
+++ js/my_swiper.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     console.log("my_swiper.js");
    let mySwiper = new Swiper('.swiper-container', {
-       speed:400,
+       speed:1500,
+       loop: true,
        spaceBetween:50,
        autoplay:{
-           delay:3000,
+           delay:2000,
        }
    });
 //    var mySwiper = new Swiper('.swiper-container', {
