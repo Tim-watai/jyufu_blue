807fb15cf3e9093f61192a1c029beb89fc0ea61a
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-04-20 14:39:36 +0800

    我的>投注紀錄 完成

--- js/my/my_bet_record.js
+++ js/my/my_bet_record.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
     console.log("my_bet_record.js");
- 
+//撤單_頁面切換
+    $("aside.my_windows_bet_record div.content ul.record li .white_red.white_red , aside.delete_windows ul .btn_group .close").click(function() {
+        console.log('message click');
+       if ($("aside.jump_window.delete_windows").hasClass("active") != true) {
+        return $("aside.jump_window.delete_windows").addClass('active');
+       }else{
+         return  $("aside.jump_window.delete_windows").removeClass('active');
+       }
+     });
+    //歷史注單與注單切換_drow down
+    $("aside .hidden_title .tack").click(function() {
+        console.log('message click');
+       if ($(this).hasClass("active") != true) {
+        return $(this).addClass('active');
+       }else{
+         return  $(this).removeClass('active');
+       }
+     });
 });//total
