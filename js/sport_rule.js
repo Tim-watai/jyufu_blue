9a9bb2f9008d177790494f96aeb6dfabb7801353
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-04-30 12:01:13 +0800

    做出體育規則 尚無下滑動內容

---
+++ js/sport_rule.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    console.log('rule ready');
+    $('aside.my_windows_help .content .li_wrap').click(function(){
+        console.log('li click');
+    })
+});//total
