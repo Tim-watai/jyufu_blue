a2ef50de547b1412237c829fc92114807ea8a640
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-08-26 17:23:11 +0800

    交易紀錄 type

--- js/my/my_bet_record.js
+++ js/my/my_bet_record.js
@@ -1,21 +1,34 @@
 $(document).ready(function(){
     console.log("my_bet_record.js");
-//撤單_頁面切換
-    $("aside.my_windows_bet_record div.content ul.record li .white_red.white_red , aside.delete_windows ul .btn_group .close").click(function() {
-        console.log('message click');
-       if ($("aside.jump_window.delete_windows").hasClass("active") != true) {
-        return $("aside.jump_window.delete_windows").addClass('active');
-       }else{
-         return  $("aside.jump_window.delete_windows").removeClass('active');
-       }
-     });
-    //歷史注單與注單切換_drow down
-    $("aside .hidden_title .tack").click(function() {
-        console.log('message click');
-       if ($(this).hasClass("active") != true) {
-        $(this).siblings().removeClass('active');
-        return $(this).addClass('active');
-       }
-       
-     });
+    let type_arr = ['.title .save','.title .get','.title .transfer'];
+    // type_arr.forEach(item => console.log(item));
+    type_arr.forEach(item => 
+      $(item).click(function(){
+        if($(this).hasClass('active') != true){
+          $(this).siblings().removeClass('active');
+          $(this).addClass('active');
+          type_ch(item);
+        }
+      })
+      );
+    function type_ch(name){
+      console.log(name);
+      if(name == '.title .save'){
+        $('.save_page').siblings().css('display','none');
+        $('.save_page').css('display','block');
+      }
+      else if(name == '.title .get'){
+        $('.get_page').siblings().css('display','none');
+        $('.get_page').css('display','block');
+      }
+      else if(name == '.title .transfer'){
+        $('.transfer_page').siblings().css('display','none');
+        $('.transfer_page').css('display','block');
+      }
+    }
+    // const callback = (item,index) => {
+    //   if(index%2===0) return;
+    //   console.log(item);
+    //  }
+    //  [0,1,2,3,4,5,6,7].forEach(callback)
 });//total
