726d12f35e6c49cd217c1a352cd4bd2f88d57e82
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-08-26 14:41:54 +0800

    drow_down

--- js/my_windows_transfer.js
+++ js/my_windows_transfer.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
     console.log("my_windows_transfer.js");
-//轉帳頁面的轉帳彈窗
-    $('aside.my_windows_mb_transfer div.content div.groups , aside.my_windows_mb_transfer_doing .windows div.content button').click(
-        function (){
-            console.log("my_windows_transfer.js");
-            if($('aside.my_windows_mb_transfer_doing').hasClass('active') != true){
-                return  $('aside.my_windows_mb_transfer_doing').addClass('active'); 
-                
-              }
-              else{
-                return $('aside.my_windows_mb_transfer_doing').removeClass('active');
-              }
+
+    let drow_down = $('.groups div.line.last.last');
+    let hidden = $('.groups .hidden');
+    let d_text = $('.groups div.line.last.last span p:first-child');
+    drow_down.click(function(){
+        console.log("click");
+        if(drow_down.parent('.groups').hasClass('active') == false){
+                   drow_down.parent('.groups').addClass('active');
+                   d_text.text('收起');
+            return hidden.slideDown('slow');
+        }else{
+                    drow_down.parent('.groups').removeClass('active');
+                    d_text.text('展開');
+            return  hidden.hide();
         }
-    );
+    })
 });//total
